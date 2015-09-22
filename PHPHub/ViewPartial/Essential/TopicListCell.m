12c8f9b9851ef9292b206aedc21421cb8de7f3ed
Aufree <freedomlijinfa@gmail.com>
2015-09-22 11:13:02 +0800

    Add auto layout to topic list cell

--- PHPHub/ViewPartial/Essential/TopicListCell.m
+++ PHPHub/ViewPartial/Essential/TopicListCell.m
@@ -7,6 +7,9 @@
 //
 
 #import "TopicListCell.h"
+#import "Masonry.h"
+
+static CGFloat topicListCellAvatarHeight = 38;
 
 @interface TopicListCell ()
 @property (nonatomic, strong) UIView *baseView;
@@ -26,13 +29,15 @@
     NSURL *URL = [BaseHelper qiniuImageCenter:_topicEntity.user.userAvatar withWidth:@"76" withHeight:@"76"];
     [_avatarImageView sd_setImageWithURL:URL];
     _topicTitleLabel.text = _topicEntity.topicTitle;
-    _topicInfoLabel.text = @"安全 • 最后由 fvzone • 4天前";
+    _topicInfoLabel.text = @"安全 • 最后由 Aufree • 5天前";
     _topicRepliesCountLabel.text = _topicEntity.topicRepliesCount.stringValue;
+    
+    [self addAutoLayoutToCell];
 }
 
 - (UIView *)baseView {
     if (!_baseView) {
-        _baseView = [[UIView alloc] initWithFrame:CGRectMake(8, 8, SCREEN_WIDTH - 16, 57)];
+        _baseView = [[UIView alloc] init];
         _baseView.backgroundColor = [UIColor whiteColor];
         _baseView.layer.cornerRadius = 2;
         _baseView.layer.masksToBounds = YES;
@@ -49,9 +54,9 @@
 
 - (UIImageView *)avatarImageView {
     if (!_avatarImageView) {
-        _avatarImageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, 38, 38)];
+        _avatarImageView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 10, topicListCellAvatarHeight, topicListCellAvatarHeight)];
         _avatarImageView.contentMode = UIViewContentModeScaleAspectFill;
-        _avatarImageView.backgroundColor = [UIColor colorWithRed:0.175 green:0.600 blue:0.953 alpha:0.340];
+        _avatarImageView.backgroundColor = [UIColor colorWithRed:0.176 green:0.600 blue:0.953 alpha:0.340];
         _avatarImageView.layer.cornerRadius = _avatarImageView.height/2;
         _avatarImageView.layer.masksToBounds = YES;
     }
@@ -60,7 +65,7 @@
 
 - (UILabel *)topicTitleLabel {
     if (!_topicTitleLabel) {
-        _topicTitleLabel = [[UILabel alloc] initWithFrame:CGRectMake(58, 10, self.baseView.width - 58 - 40, 17)];
+        _topicTitleLabel = [[UILabel alloc] init];
         _topicTitleLabel.font = [UIFont fontWithName:FontName size:14];
         _topicTitleLabel.numberOfLines = 1;
     }
@@ -69,7 +74,8 @@
 
 - (UILabel *)topicInfoLabel {
     if (!_topicInfoLabel) {
-        _topicInfoLabel = [[UILabel alloc] initWithFrame:CGRectMake(58, self.contentView.height - 10, self.baseView.width - 58 - 40, 15)];
+        _topicInfoLabel = [[UILabel alloc] init];
+        _topicInfoLabel.height = 15;
         _topicInfoLabel.font = [UIFont fontWithName:FontName size:11];
         _topicInfoLabel.numberOfLines = 1;
         _topicInfoLabel.textColor = [UIColor colorWithWhite:0.773 alpha:1.000];
@@ -79,7 +85,7 @@
 
 - (UILabel *)topicRepliesCountLabel {
     if (!_topicRepliesCountLabel) {
-        _topicRepliesCountLabel = [[UILabel alloc] initWithFrame:CGRectMake(self.baseView.width - 30, (self.baseView.height - 20)/2, 20, 20)];
+        _topicRepliesCountLabel = [[UILabel alloc] init];
         _topicRepliesCountLabel.font = [UIFont fontWithName:FontName size:11];
         _topicRepliesCountLabel.numberOfLines = 1;
         _topicRepliesCountLabel.textColor = [UIColor whiteColor];
@@ -89,5 +95,30 @@
         _topicRepliesCountLabel.layer.masksToBounds = YES;
     }
     return _topicRepliesCountLabel;
+}
+
+- (void)addAutoLayoutToCell {
+    CGFloat baseViewMargin = 8;
+    CGFloat topicTitleMargin = 10;
+    CGFloat topicTitleOffset = self.avatarImageView.width + topicTitleMargin * 2;
+    
+    [self.baseView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.offset(baseViewMargin);
+        make.left.equalTo(self.contentView.mas_left).offset(baseViewMargin);
+        make.right.equalTo(self.contentView.mas_right).offset(-baseViewMargin);
+        make.bottom.offset(0);
+    }];
+    
+    [self.topicTitleLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.baseView.mas_top).offset(topicTitleMargin);
+        make.left.equalTo(self.baseView.mas_left).offset(topicTitleOffset);
+        make.right.equalTo(self.baseView.mas_right).offset(-topicTitleMargin);
+    }];
+    
+    [self.topicInfoLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.baseView.mas_bottom).offset(-topicTitleMargin - self.topicInfoLabel.height);
+        make.left.equalTo(self.baseView.mas_left).offset(topicTitleOffset);
+        make.right.equalTo(self.baseView.mas_right).offset(-topicTitleMargin);
+    }];
 }
 @end
