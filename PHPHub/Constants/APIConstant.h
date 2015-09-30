cef7779935fa6469d06eb111e0d1634dbe8a059c
Aufree <freedomlijinfa@gmail.com>
2015-09-30 10:37:57 +0800

    Add secret constant example

--- PHPHub/Constants/APIConstant.h
+++ PHPHub/Constants/APIConstant.h
@@ -1,18 +1,14 @@
 //
-//  APIConstant.example.h
+//  APIConstant.h
 //  PHPHub
 //
 //  Created by Aufree on 9/30/15.
 //  Copyright (c) 2015 ESTGroup. All rights reserved.
 //
 
-#define Client_id                @"get client id from server"
-#define Client_secret            @"get client secret id from server"
 #define APIAccessToken [NSString stringWithFormat:@"%@%@", APIBaseURL, @"oauth/access_token"]
 
 #define APIBaseURL                 @"https://api.phphub.org"
 #define APIClientTokenIdentifier   @"APIClientTokenIdentifier"
 #define APIPasswordTokenIdentifier @"APIPasswordTokenIdentifier"
 #define QiniuUploadTokenIdentifier @"QiniuUploadTokenIdentifier"
-
-#define UMENG_APPKEY @"setup umeng appkey"
---
+++ PHPHub/Constants/SecretConstant.example.h
@@ -0,0 +1,11 @@
+//
+//  SecretConstant.example.h
+//  PHPHub
+//
+//  Created by Aufree on 9/30/15.
+//  Copyright (c) 2015 ESTGroup. All rights reserved.
+//
+
+#define Client_id     @"Get the client id from server"
+#define Client_secret @"Get the client secret from server"
+#define UMENG_APPKEY  @"Set up UMEng App Key"
