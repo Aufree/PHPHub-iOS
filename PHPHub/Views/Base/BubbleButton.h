49e0e7c406bb796a689816c59ec4dddf6f06f9b5
Aufree <freedomlijinfa@gmail.com>
2015-10-09 11:54:14 +0800

    Add bubble button animation

---
+++ PHPHub/Views/Base/BubbleButton.h
@@ -0,0 +1,13 @@
+//
+//  BubbleButton.h
+//  PHPHub
+//
+//  Created by Aufree on 10/9/15.
+//  Copyright © 2015 ESTGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BubbleButton : UIButton
+
+@end
