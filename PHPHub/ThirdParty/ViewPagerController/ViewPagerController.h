bdceea786ebc46baf84264c905e0b8ebfea69b4d
Aufree <freedomlijinfa@gmail.com>
2015-10-13 16:42:50 +0800

    Improve ViewPagerController

--- PHPHub/ThirdParty/ViewPagerController/ViewPagerController.h
+++ PHPHub/ThirdParty/ViewPagerController/ViewPagerController.h
@@ -31,23 +31,11 @@ typedef NS_ENUM(NSUInteger, ViewPagerComponent) {
 @property (nonatomic, weak) id<ViewPagerDataSource> dataSource;
 @property (nonatomic, weak) id<ViewPagerDelegate> delegate;
 
-@property (nonatomic) UIScrollView *origPageScrollView;
-
 @property UIPageViewController *pageViewController;
 
 - (UIViewController *)viewControllerAtIndex:(NSUInteger)index;
 
 #pragma mark ViewPagerOptions
-// Tab bar's height, defaults to 49.0
-@property CGFloat tabHeight;
-// Tab bar's offset from left, defaults to 56.0
-@property CGFloat tabOffset;
-// Any tab item's width, defaults to 128.0. To-do: make this dynamic
-@property CGFloat tabWidth;
-
-// 1.0: Top, 0.0: Bottom, changes tab bar's location in the screen
-// Defaults to Top
-@property CGFloat tabLocation;
 
 // 1.0: YES, 0.0: NO, defines if view should appear with the second or the first tab
 // Defaults to NO
@@ -58,9 +46,6 @@ typedef NS_ENUM(NSUInteger, ViewPagerComponent) {
 @property CGFloat centerCurrentTab;
 
 #pragma mark Colors
-// Colors for several parts
-@property UIColor *indicatorColor;
-@property UIColor *tabsViewBackgroundColor;
 @property UIColor *contentViewBackgroundColor;
 
 @property (nonatomic) BOOL manualLoadData;
@@ -93,16 +78,6 @@ typedef NS_ENUM(NSUInteger, ViewPagerComponent) {
 // Every time - reloadData called, ViewPager will ask its delegate for option values
 // So you don't have to set options from ViewPager itself
 - (CGFloat)viewPager:(ViewPagerController *)viewPager valueForOption:(ViewPagerOption)option withDefault:(CGFloat)value;
-/*
- * Use this method to customize the look and feel.
- * viewPager will ask its delegate for colors for its components.
- * And if they are provided, it will use them, otherwise it will use default colors.
- * Also not that, colors for tab and content views will change the tabView's and contentView's background
- * (you should provide these views with a clearColor to see the colors),
- * and indicator will change its own color.
- */
-
-- (UIColor *)viewPager:(ViewPagerController *)viewPager colorForComponent:(ViewPagerComponent)component withDefault:(UIColor *)color;
 
 - (void)setSubViewScrollStatus:(BOOL)enabled;
 @end
--- PHPHub/ThirdParty/ViewPagerController/ViewPagerController.m
+++ PHPHub/ThirdParty/ViewPagerController/ViewPagerController.m
@@ -10,21 +10,14 @@
 
 @interface ViewPagerController () <UIScrollViewDelegate>
 
-@property (nonatomic, weak) id<UIScrollViewDelegate> origPageScrollViewDelegate;
-
-@property UIScrollView *tabsView;
 @property UIView *contentView;
-@property UIView *maskView;
 
-@property NSMutableArray *tabs;
-@property NSMutableArray *tabsLabelWidthArray;
 @property NSMutableArray *contents;
 
 @property NSUInteger tabCount;
 @property NSUInteger currentIndex;
 @property (getter = isAnimatingToTab, assign) BOOL animatingToTab;
 
-@property (nonatomic) NSUInteger activeTabIndex;
 @property (nonatomic) BOOL canLimitBounce;
 
 @end
@@ -61,93 +54,8 @@
     [[NSNotificationCenter defaultCenter] removeObserver:self];
 }
 
-- (void)viewWillLayoutSubviews {
-    
-    CGRect frame;
-    
-    frame = _tabsView.frame;
-    frame.origin.x = 0.0;
-    frame.origin.y = self.tabLocation ? 0.0 : self.view.frame.size.height - self.tabHeight;
-    frame.size.width = self.view.bounds.size.width;
-    frame.size.height = self.tabHeight;
-    _tabsView.frame = frame;
-    
-    frame = _contentView.frame;
-    frame.origin.x = 0.0;
-    frame.origin.y = self.tabLocation ? self.tabHeight : 0.0;
-    frame.size.width = self.view.bounds.size.width;
-    frame.size.height = self.view.frame.size.height - self.tabHeight;
-    _contentView.frame = frame;
-}
-
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-}
-
-- (IBAction)handleTapGesture:(id)sender {
-    
-    self.animatingToTab = YES;
-    
-    // Get the desired page's index
-    UITapGestureRecognizer *tapGestureRecognizer = (UITapGestureRecognizer *)sender;
-    UIView *tabView = tapGestureRecognizer.view;
-    __block NSUInteger index = [_tabs indexOfObject:tabView];
-    
-    // Get the desired viewController
-    UIViewController *viewController = [self viewControllerAtIndex:index];
-    
-    // __weak pageViewController to be used in blocks to prevent retaining strong reference to self
-    __weak UIPageViewController *weakPageViewController = self.pageViewController;
-    __weak ViewPagerController *weakSelf = self;
-    
-    self.canLimitBounce = NO;
-    if (index < self.activeTabIndex) {
-        [self.pageViewController setViewControllers:@[viewController]
-                                          direction:UIPageViewControllerNavigationDirectionReverse
-                                           animated:YES
-                                         completion:^(BOOL completed) {
-                                             weakSelf.animatingToTab = NO;
-                                             
-                                             // Set the current page again to obtain synchronisation between tabs and content
-                                             dispatch_async(dispatch_get_main_queue(), ^{
-                                                 [weakPageViewController setViewControllers:@[viewController]
-                                                                                  direction:UIPageViewControllerNavigationDirectionReverse
-                                                                                   animated:NO
-                                                                                 completion:nil];
-                                             });
-                                             if (completed == YES) {
-                                                 weakSelf.canLimitBounce = YES;
-                                             }
-                                         }];
-    } else if (index > self.activeTabIndex) {
-        [self.pageViewController setViewControllers:@[viewController]
-                                          direction:UIPageViewControllerNavigationDirectionForward
-                                           animated:YES
-                                         completion:^(BOOL completed) {
-                                             weakSelf.animatingToTab = NO;
-                                             
-                                             // Set the current page again to obtain synchronisation between tabs and content
-                                             dispatch_async(dispatch_get_main_queue(), ^{
-                                                 [weakPageViewController setViewControllers:@[viewController]
-                                                                                  direction:UIPageViewControllerNavigationDirectionForward
-                                                                                   animated:NO
-                                                                                 completion:nil];
-                                             });
-                                             if (completed == YES) {
-                                                 weakSelf.canLimitBounce = YES;
-                                             }
-                                         }];
-    }
-    
-    // Set activeTabIndex
-    self.activeTabIndex = index;
-}
-
-#pragma mark -
-- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation {
-    
-    // Re-align tabs if needed
-    self.activeTabIndex = self.activeTabIndex;
 }
 
 #pragma mark -
@@ -156,9 +64,7 @@
     _pageViewController = [[UIPageViewController alloc] initWithTransitionStyle:UIPageViewControllerTransitionStyleScroll
                                                           navigationOrientation:UIPageViewControllerNavigationOrientationHorizontal
                                                                         options:nil];
-    //Setup some forwarding events to hijack the scrollview
-    self.origPageScrollView = (UIScrollView*)[_pageViewController.view.subviews objectAtIndex:0];
-    self.origPageScrollViewDelegate = self.origPageScrollView.delegate;
+
     [((UIScrollView*)[_pageViewController.view.subviews objectAtIndex:0]) setDelegate:self];
     
     _pageViewController.dataSource = self;
@@ -171,49 +77,15 @@
     
     _currentIndex = 0;
     _canLimitBounce = YES;
-    
-    // Empty tabs and contents
-    [_tabs removeAllObjects];
+
     [_contents removeAllObjects];
-    [_tabsLabelWidthArray removeAllObjects];
     
     _tabCount = [self.dataSource numberOfTabsForViewPager:self];
-    
-    // Populate arrays with [NSNull null];
-    _tabs = [NSMutableArray arrayWithCapacity:_tabCount];
-    for (int i = 0; i < _tabCount; i++) {
-        [_tabs addObject:[NSNull null]];
-    }
     
     _contents = [NSMutableArray arrayWithCapacity:_tabCount];
     for (int i = 0; i < _tabCount; i++) {
         [_contents addObject:[NSNull null]];
     }
-    
-    _tabsLabelWidthArray = [NSMutableArray arrayWithCapacity:_tabCount];
-    for (int i = 0; i < _tabCount; i++) {
-        [_tabsLabelWidthArray addObject:[NSNull null]];
-    }
-    
-    // Add tabsView
-    _tabsView = [[UIScrollView alloc] initWithFrame:CGRectMake(0.0, 0.0, self.view.frame.size.width, self.tabHeight)];
-    _tabsView.autoresizingMask = UIViewAutoresizingFlexibleWidth;
-    _tabsView.backgroundColor = self.tabsViewBackgroundColor;
-    _tabsView.showsHorizontalScrollIndicator = NO;
-    _tabsView.showsVerticalScrollIndicator = NO;
-    [self.view insertSubview:_tabsView atIndex:0];
-    
-    // @Jin hack for swipe back in topic list view controller and user list view controller
-    _maskView =[[UIView alloc] initWithFrame:CGRectMake(0, self.tabHeight, 15, SCREEN_HEIGHT)];
-    _maskView.backgroundColor = [[UIColor whiteColor] colorWithAlphaComponent:0.1f];
-    _maskView.userInteractionEnabled = YES;
-    //    [self.view addSubview:_maskView];
-    
-    // Add tab views to _tabsView
-    CGFloat contentSizeWidth = 0;
-    
-    _tabsView.contentSize = CGSizeMake(contentSizeWidth, self.tabHeight);
-    
     
     if (!_contentView) {
         _contentView = _pageViewController.view;
@@ -242,9 +114,6 @@
                                   direction:UIPageViewControllerNavigationDirectionForward
                                    animated:NO
                                  completion:nil];
-    
-    // Set activeTabIndex
-    self.activeTabIndex = self.startFromSecondTab;
 }
 
 - (UIViewController *)viewControllerAtIndex:(NSUInteger)index {
@@ -259,12 +128,6 @@
         
         if ([self.dataSource respondsToSelector:@selector(viewPager:contentViewControllerForTabAtIndex:)]) {
             viewController = [self.dataSource viewPager:self contentViewControllerForTabAtIndex:index];
-        } else if ([self.dataSource respondsToSelector:@selector(viewPager:contentViewForTabAtIndex:)]) {
-            
-            UIView *view = [self.dataSource viewPager:self contentViewForTabAtIndex:index];
-            
-            viewController = [UIViewController new];
-            viewController.view = view;
         } else {
             viewController = [[UIViewController alloc] init];
             viewController.view = [[UIView alloc] init];
@@ -285,11 +148,6 @@
 - (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerAfterViewController:(UIViewController *)viewController {
     NSUInteger index = [self indexForViewController:viewController];
     self.currentIndex = index;
-    if (index == 0) {
-        self.maskView.userInteractionEnabled = YES;
-    } else {
-        self.maskView.userInteractionEnabled = NO;
-    }
     index++;
     return [self viewControllerAtIndex:index];
 }
@@ -297,24 +155,8 @@
 - (UIViewController *)pageViewController:(UIPageViewController *)pageViewController viewControllerBeforeViewController:(UIViewController *)viewController {
     NSUInteger index = [self indexForViewController:viewController];
     self.currentIndex = index;
-    if (index == 0) {
-        self.maskView.userInteractionEnabled = YES;
-    } else {
-        self.maskView.userInteractionEnabled = NO;
-    }
     index--;
     return [self viewControllerAtIndex:index];
-}
-
-#pragma mark - UIPageViewControllerDelegate
-- (void)pageViewController:(UIPageViewController *)pageViewController willTransitionToViewControllers:(NSArray *)pendingViewControllers {
-    
-    //    NSLog(@"willTransitionToViewController: %i", [self indexForViewController:[pendingViewControllers objectAtIndex:0]]);
-}
-- (void)pageViewController:(UIPageViewController *)pageViewController didFinishAnimating:(BOOL)finished previousViewControllers:(NSArray *)previousViewControllers transitionCompleted:(BOOL)completed {
-    
-    UIViewController *viewController = self.pageViewController.viewControllers[0];
-    self.activeTabIndex = [self indexForViewController:viewController];
 }
 
 #pragma mark - UIScrollViewDelegate, Responding to Scrolling and Dragging
@@ -322,10 +164,6 @@
     
     if (self.scrollingLocked) {
         return;
-    }
-    
-    if ([self.origPageScrollViewDelegate respondsToSelector:@selector(scrollViewDidScroll:)]) {
-        [self.origPageScrollViewDelegate scrollViewDidScroll:scrollView];
     }
     
     if (self.currentIndex == 0 && scrollView.contentOffset.x < SCREEN_WIDTH && self.canLimitBounce == YES) {
@@ -336,20 +174,12 @@
 }
 
 - (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
-    if ([self.origPageScrollViewDelegate respondsToSelector:@selector(scrollViewWillBeginDragging:)]) {
-        [self.origPageScrollViewDelegate scrollViewWillBeginDragging:scrollView];
-    }
-    
     if (self.delegate && [self.delegate respondsToSelector:@selector(setSubViewScrollStatus:)]) {
         [self.delegate setSubViewScrollStatus:NO];
     }
 }
 
 - (void)scrollViewWillEndDragging:(UIScrollView *)scrollView withVelocity:(CGPoint)velocity targetContentOffset:(inout CGPoint *)targetContentOffset {
-    if ([self.origPageScrollViewDelegate respondsToSelector:@selector(scrollViewWillEndDragging:withVelocity:targetContentOffset:)]) {
-        [self.origPageScrollViewDelegate scrollViewWillEndDragging:scrollView withVelocity:velocity targetContentOffset:targetContentOffset];
-    }
-    
     if (self.delegate && [self.delegate respondsToSelector:@selector(setSubViewScrollStatus:)]) {
         [self.delegate setSubViewScrollStatus:YES];
     }
