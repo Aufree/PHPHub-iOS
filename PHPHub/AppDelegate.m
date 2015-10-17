02efd592b200c3a234085bea85ca681817228baa
Aufree <freedomlijinfa@gmail.com>
2015-10-17 15:54:41 +0800

    Update app badge value when app did become active

--- PHPHub/AppDelegate.m
+++ PHPHub/AppDelegate.m
@@ -72,7 +72,7 @@
 }
 
 - (void)applicationDidBecomeActive:(UIApplication *)application {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application {
