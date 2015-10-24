0792fc5f0e66031338143ee8ed46a91907b37f81
Aufree <freedomlijinfa@gmail.com>
2015-10-24 09:38:05 +0800

    Change scan to login guide url

--- PHPHub/Constants/APIConstant.h
+++ PHPHub/Constants/APIConstant.h
@@ -22,6 +22,6 @@
 #define ProjectURL      @"https://github.com/phphub/phphub-ios"
 #define AboutPageURL    @"https://phphub.org/about"
 #define ESTGroupURL     @"http://est-group.org"
-#define PHPHubGuide     @"https://phphub.org/helps/qr-login-guide"
+#define PHPHubGuide     @"http://7xnqwn.com1.z0.glb.clouddn.com/index.html"
 #define PHPHubTopicURL  @"https://phphub.org/topics/"
 #define SinaRedirectURL @"http://sns.whalecloud.com/sina2/callback"
