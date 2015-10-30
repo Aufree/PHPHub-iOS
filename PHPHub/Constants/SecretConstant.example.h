83e16fa7755f2b4c908366d28aa83180a2d9e817
Aufree <freedomlijinfa@gmail.com>
2015-10-30 09:14:22 +0800

    Update comments of SecretConstant file

--- PHPHub/Constants/SecretConstant.example.h
+++ PHPHub/Constants/SecretConstant.example.h
@@ -10,13 +10,14 @@
 #define Client_id       @"kHOugsx4dmcXwvVbmLkd"
 #define Client_secret   @"PuuFCrF94MloSbSkxpwS"
 #else
-#define Client_id       @"Set up a client id for production env"
-#define Client_secret   @"Set up a client secret for production env"
+#define Client_id       @"" // Set up a client id for production environment
+#define Client_secret   @"" // Set up a client secret for production environment
 #endif
 
-#define UMENG_APPKEY    @"Set up UMEng App Key"
-#define UMENG_QQ_ID     @"Set up qq id"
-#define UMENG_QQ_APPKEY @"Set up qq appkey"
-#define WX_APP_ID       @"Set up weixin app id"
-#define WX_APP_SECRET   @"Set up weixin app secret"
-#define TRACKING_ID     @"Set up google anlytics tracking id"
+#define UMENG_APPKEY    @"" // Set up UMEng App Key
+#define UMENG_QQ_ID     @"" // Set up QQ id
+#define UMENG_QQ_APPKEY @"" // Set up QQ appkey
+#define WX_APP_ID       @"" // Set up wechat app id
+#define WX_APP_SECRET   @"" // Set up wechat app secret
+#define JPush_APP_KEY   @"" // Set up Jpush App Key
+#define TRACKING_ID     @"" // Set up google anlytics tracking id
