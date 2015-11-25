c0cedfe94c682324df529e1e79b5c2d1f8327341
Aufree <freedomlijinfa@gmail.com>
2015-11-26 00:02:40 +0800

    Improve AppDelegate

--- PHPHub/AppDelegate.m
+++ PHPHub/AppDelegate.m
@@ -23,18 +23,53 @@
 
 @implementation AppDelegate
 
+#pragma mark - Application delegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     
+    // Showing the App
+    [self makeWindowVisible:launchOptions];
+    
+    // Basic setup
+    [self basicSetup];
+
+    return YES;
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    [LaunchScreenAdHandler checkShouldShowLaunchScreenAd];
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
+    [[CurrentUser Instance] checkNoticeCount];
+}
+
+#pragma mark - Make window visible
+
+- (void)makeWindowVisible:(NSDictionary *)launchOptions {
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    self.window.backgroundColor = [UIColor whiteColor];
+    
+    [[UINavigationBar appearance] setBarTintColor:[UIColor whiteColor]];
+    
+    if (_tabBarViewController == nil){
+        _tabBarViewController = [[BaseTabBarViewController alloc] init];
+    }
+    self.window.rootViewController = _tabBarViewController;
+    
+    [self.window makeKeyAndVisible];
+}
+
+#pragma mark - Basic setup
+
+- (void)basicSetup {
     // About feedback
     [UMFeedback setAppkey:UMENG_APPKEY];
     [UMOpus setAudioEnable:YES];
     
     // Exception Collection
     [ExceptionHandler bootup];
-    
-    // Showing the App
-    [self makeWindowVisible:launchOptions];
     
     // Change UITextField and UITextView Cursor / Caret Color
     [[UITextField appearance] setTintColor:[UIColor grayColor]];
@@ -45,22 +80,6 @@
     
     // Tracking Server Bootup
     [AnalyticsHandler bootup];
-
-    return YES;
-}
-
-- (void)makeWindowVisible:(NSDictionary *)launchOptions {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    self.window.backgroundColor = [UIColor whiteColor];
-    
-    [[UINavigationBar appearance] setBarTintColor:[UIColor whiteColor]];
-
-    if (_tabBarViewController == nil){
-        _tabBarViewController = [[BaseTabBarViewController alloc] init];
-    }
-    self.window.rootViewController = _tabBarViewController;
-    
-    [self.window makeKeyAndVisible];
     
     // Setup Jpush
     [JpushHandler sendUserIdToAlias];
@@ -81,28 +100,7 @@
     [SSKeychain setAccessibilityType:kSecAttrAccessibleWhenUnlocked];
 }
 
-- (void)applicationWillResignActive:(UIApplication *)application {
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application {
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application {
-    [LaunchScreenAdHandler checkShouldShowLaunchScreenAd];
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application {
-    [[UIApplication sharedApplication] setApplicationIconBadgeNumber:0];
-    [[CurrentUser Instance] checkNoticeCount];
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application {
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-}
+#pragma mark - APNs
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken {
     // Jpush
@@ -117,6 +115,8 @@
         [JpushHandler handleUserInfo:userInfo];
     }
 }
+
+#pragma mark - Handle url scheme
 
 - (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
     return [UMSocialSnsService handleOpenURL:url];
@@ -150,6 +150,7 @@
 }
 
 #pragma mark - Status bar touch tracking
+
 - (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
     [super touchesBegan:touches withEvent:event];
     CGPoint location = [[[event allTouches] anyObject] locationInView:[self window]];
